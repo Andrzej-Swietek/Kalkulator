2551e512da7df273bd4c0655c8b38750dc114d1b
AndrzejSwietek2001 <37212487+AndrzejSwietek2001@users.noreply.github.com>
2018-04-10 21:32:35 +0200

    zarys Panelu macierzy

---
+++ kalkulator/kalkulator/Panels/PanelMatrix.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace kalkulator.Panels
+{
+    partial class PanelMatrix
+    {
+        /// <summary> 
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod wygenerowany przez Projektanta składników
+
+        /// <summary> 
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować 
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(17, 68);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Dodawanie";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(17, 112);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 38);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Odejmowanie";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(15, 156);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(133, 38);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Mnożenie";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(15, 200);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(133, 38);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Skalar";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.checkBox1.Location = new System.Drawing.Point(17, 304);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(138, 29);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Tag = "Det";
+            this.checkBox1.Text = "Wyznacznik";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(15, 244);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(134, 38);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Macierz Transponowana";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // PanelMatrix
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "PanelMatrix";
+            this.Size = new System.Drawing.Size(553, 373);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button5;
+    }
+}
